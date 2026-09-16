7c70e79547422950dfd6fae9e6f91212d634c785
Waris Khamkaweepart <savewaris@gmail.com>
2026-09-16 19:56:38 +0700

    A5: green - unit field added, all tests updated

--- test_kitchen.py
+++ test_kitchen.py
@@ -1,13 +1,13 @@
 from kitchen import Quantity
 
 def test_multiplication_returns_a_new_quantity():
-    flour = Quantity(200)
-    assert flour.times(3) == Quantity(600)
-    assert flour.times(2) == Quantity(400)
-    
+    flour = Quantity(200, "g")
+    assert flour.times(3) == Quantity(600, "g")
+    assert flour.times(2) == Quantity(400, "g")
+
 def test_equality():
-    assert Quantity(200) == Quantity(200)
-    assert Quantity(200) != Quantity(300)
-  
+    assert Quantity(200, "g") == Quantity(200, "g")
+    assert Quantity(200, "g") != Quantity(300, "g")
+
 def test_grams_are_not_ounces():
     assert Quantity(1, "g") != Quantity(1, "oz")
